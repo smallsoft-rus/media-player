7fdde9b5b61c3f1e15b1ee69db8702039821c706
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-05-09 14:35:21 +0500

    Add tests project

---
+++ SmallMediaPlayer_Tests/tags_tests.cpp
@@ -0,0 +1,21 @@
+/* Small Media Player tests 
+ * Copyright (c) 2021,  MSDN.WhiteKnight (https://github.com/smallsoft-rus/media-player) 
+ * License: BSD 2.0 */
+#include "CppUnitTest.h"
+#include "tags.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SmallMediaPlayer_Tests
+{		
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+	
+        TEST_METHOD(TestMethod1)
+        {
+            // TODO: Разместите здесь код своего теста
+        }
+
+    };
+}
