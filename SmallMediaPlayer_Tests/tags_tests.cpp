28a2e93b476067fde172b269dec13bece45770ad
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-05-09 15:26:14 +0500

    Add test for ReadTagsV1

--- SmallMediaPlayer_Tests/tags_tests.cpp
+++ SmallMediaPlayer_Tests/tags_tests.cpp
@@ -1,4 +1,4 @@
-/* Small Media Player tests 
+п»ї/* Small Media Player tests 
  * Copyright (c) 2021,  MSDN.WhiteKnight (https://github.com/smallsoft-rus/media-player) 
  * License: BSD 2.0 */
 #include "CppUnitTest.h"
@@ -6,15 +6,23 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+WCHAR Noise_MP3[]=L"..\\SmallMediaPlayer_Tests\\data\\noise.mp3";
+
 namespace SmallMediaPlayer_Tests
 {		
-    TEST_CLASS(UnitTest1)
+    TEST_CLASS(TagTests)
     {
     public:
 	
-        TEST_METHOD(TestMethod1)
+        TEST_METHOD(Test_ID3V1)
         {
-            // TODO: Разместите здесь код своего теста
+            TAGS_GENERIC data = {0};
+            BOOL res = ReadTagsV1(Noise_MP3,&data);
+            Assert::IsTrue(res!=FALSE);
+            Assert::AreEqual((DWORD)TAG_ID3V1,(DWORD)data.type);
+            Assert::AreEqual(L"РЁСѓРј РјРѕСЂСЏ",data.title);
+            Assert::AreEqual(L"Gregory",data.artist);
+            Assert::AreEqual(L"",data.album);
         }
 
     };
