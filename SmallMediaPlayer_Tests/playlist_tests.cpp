e474351dc9492ea0084f723deeaeb3d0584e0e70
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-04-27 10:03:11 +0500

    Disable errors GUI in tests

--- SmallMediaPlayer_Tests/playlist_tests.cpp
+++ SmallMediaPlayer_Tests/playlist_tests.cpp
@@ -9,7 +9,7 @@
 #include "player.h"
 
 //imported from ui.obj
-extern void InitApplication();
+extern void InitApplication(BOOL useErrorGUI);
 extern void InitResources(HMODULE h);
 
 //imported from RegistryModule.obj
@@ -28,7 +28,7 @@ void TestsMain(){
     Init_ProgramFileName();
     //tests must fetch resources from DLL instead of EXE
     InitResources(GetModuleHandle(L"SmallMediaPlayer_Tests.dll"));
-    InitApplication();
+    InitApplication(FALSE);
     
     TestsMain_Initialized=true;
 }
